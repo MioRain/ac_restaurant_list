c8768608ac059f9c1ff05ae641d107b6e93c4112
MioRain <ericzero1593@hotmail.com>
2022-06-19 22:51:52 +0800

    modify restaurantSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,16 +1,50 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const db = require('../../config/mongoose')
 const Restaurant = require('../Restaurant')
+const User = require('../User')
 const restaurantList = require('../../restaurant.json').results
 
-db.once('open', () => {
-  console.log('Creating seeds......')
+const SEED_USERS = [
+  {
+    name: 'user1',
+    email: 'user1@example.com',
+    password: '12345678',
+    restaurants: restaurantList.slice(0, 3)
+  },
+  {
+    name: 'user2',
+    email: 'user2@example.com',
+    password: '12345678',
+    restaurants: restaurantList.slice(3, 6)
+  }
+]
 
-  Restaurant.create(restaurantList)
+db.once('open', () => {
+  // 先創建 users
+  return Promise.all(Array.from(SEED_USERS, seedUser => {
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(seedUser.password, salt))
+      .then(hash => User.create({
+        name: seedUser.name,
+        email: seedUser.email,
+        password: hash
+      }))
+      // 創建餐廳資料
+      .then(user => {
+        const userId = user._id
+        return Promise.all(Array.from(seedUser.restaurants, restaurant => {
+          restaurant.userId = userId
+          return Restaurant.create(restaurant)
+        }))
+      })
+  }))
     .then(() => {
-      console.log("restaurantSeeder done!")
+      console.log('done')
+      process.exit()
     })
     .catch(err => console.log(err))
-    .finally(() => db.close())
 })
-
-module.exports = db
